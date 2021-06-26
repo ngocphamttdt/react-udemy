e3f072c4dd066215a80c5078d8e33cb8ae7c7896
Pham Thi Thuy Ngoc <ngoc.pham@idealogic.com.vn>
2021-06-26 12:18:11 +0700

    Section 9: Asynchronous code

--- src/index.js
+++ src/index.js
@@ -10,9 +10,12 @@ import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { configureStore } from './app/store/configureStore'
 import ScrollToTop from './app/layout/ScrollToTop'
+import { loadEvents } from './features/events/eventActions'
 
 
 const store = configureStore()
+store.dispatch(loadEvents())
+
 
 init(render)
 
