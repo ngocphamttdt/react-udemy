2e145bfda5b4fec3234eba6ed01c4b3ff4a11fce
Pham Thi Thuy Ngoc <ngoc.pham@idealogic.com.vn>
2021-06-28 00:04:35 +0700

    Section 10: Introduction to Firestore

---
+++ src/app/config/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/database'
+import 'firebase/auth'
+import 'firebase/storage'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBJjXs7D-zGsA5mWbktNQnpiMdFJkmDYRs",
+  authDomain: "reventscourse-25367.firebaseapp.com",
+  projectId: "reventscourse-25367",
+  storageBucket: "reventscourse-25367.appspot.com",
+  messagingSenderId: "769597807252",
+  appId: "1:769597807252:web:3698e566b3fe2642783281"
+}
+
+firebase.initializeApp(firebaseConfig)
+firebase.firestore()
