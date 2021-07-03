5e722beb47cf0025b927364092e61aef25044947
Pham Thi Thuy Ngoc <ngoc.pham@idealogic.com.vn>
2021-07-03 15:26:55 +0700

    Section 10: Introduction to Firestore

--- src/app/config/firebase.js
+++ src/app/config/firebase.js
@@ -15,3 +15,5 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig)
 firebase.firestore()
+
+export default firebase
--- src/features/events/eventDashboard/EventDasboard.jsx
+++ src/features/events/eventDashboard/EventDasboard.jsx
@@ -2,30 +2,39 @@ import React from 'react'
 import { Grid } from 'semantic-ui-react'
 import EvenList from './EventList'
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import EventListItemPlaceholder from './EventListItemPlaceholder'
 import EventFilters from './EventFilters'
+import { listenToEventsFormFirestore } from '../../../app/firestore/fireStoreService';
+import { listenToEvents } from '../eventActions';
+
+import useFirestoreCollection from '../../../app/hooks/useFirestoreCollection';
 
 const EventDashboard = () => {
 
+	const dispatch = useDispatch()
 	const { events } = useSelector(state => state.event)
-	const {loading} = useSelector(state => state.async)
+	const { loading } = useSelector(state => state.async)
 
+	useFirestoreCollection({
+		query: () => listenToEventsFormFirestore(),
+		data: events => dispatch(listenToEvents(events)),
+		deps: [dispatch]
+	})
 
-	
 	return (
 		<Grid>
 			<Grid.Column width={10}>
 				{loading &&
-				<>
-				<EventListItemPlaceholder/>
-				<EventListItemPlaceholder/>
-				</>
+					<>
+						<EventListItemPlaceholder />
+						<EventListItemPlaceholder />
+					</>
 				}
 				<EvenList events={events} />
 			</Grid.Column>
 			<Grid.Column width={6}>
-			<EventFilters/>
+				<EventFilters />
 			</Grid.Column>
 		</Grid>
 	);
