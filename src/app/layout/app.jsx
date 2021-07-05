3ce754f7e49bb7456143a02dc5a205b542cf1783
Pham Thi Thuy Ngoc <ngoc.pham@idealogic.com.vn>
2021-07-05 09:57:04 +0700

    Section 11: Authentication

--- src/app/layout/app.jsx
+++ src/app/layout/app.jsx
@@ -10,10 +10,15 @@ import Sandbox from '../../features/sanbox/Sandbox'
 import ModalManager from '../common/modals/ModalManager';
 import { ToastContainer } from 'react-toastify'
 import ErrorComponent from '../common/errors/ErrorComponent';
+import AccountPage from '../../features/auth/AccountPage';
+import { useSelector } from 'react-redux';
+import LoadingComponent from './LoadingComponent';
 
 export default function App() {
 	const { key } = useLocation()
+	const { initialized } = useSelector((state) => state.async)
 
+	if (!initialized) return <LoadingComponent content='Loading app ....' />
 	return (
 		<>
 			<ModalManager />
@@ -29,6 +34,7 @@ export default function App() {
 							<Route exact path='/sandbox' component={Sandbox} />
 							<Route exact path='/events/:id' component={EvenDetailedPage} />
 							<Route exact path={['/createEvent', '/manage/:id']} component={EventForm} key={key} />
+							<Route path='/account' component={AccountPage} />
 							<Route path='/error' component={ErrorComponent} />
 						</Container>
 					</>
